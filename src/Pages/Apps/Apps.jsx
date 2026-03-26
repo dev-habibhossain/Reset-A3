099d6d115ae15a22800d72c0b319b8679b8f87ab
Habib Hossain <habibfahim71@gmail.com>
2026-03-26 20:16:45 +0600

    'setup'

---
+++ src/Pages/Apps/Apps.jsx
@@ -0,0 +1,10 @@
+
+const Apps = () => {
+    return (
+        <div>
+            <h1>This is Apps page...</h1>
+        </div>
+    );
+};
+
+export default Apps; 
--- src/Routes/Route.jsx
+++ src/Routes/Route.jsx
@@ -1,7 +1,8 @@
 import { createBrowserRouter } from "react-router";
 import Home from "../Pages/Home/Home";
 import MainLayout from "../Layouts/MainLayout";
-import Products from "../Pages/Products/Products";
+import Error from "../Pages/Error/Error";
+import Apps from "../Pages/Apps/Apps";
 
 
 
@@ -9,14 +10,15 @@ export const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout />,
+    errorElement: <Error />,
     children:[
         {
             path: '/',
             element: <Home />
         },
         {
-            path: '/products',
-            element: <Products />
+            path: '/apps',
+            element: <Apps />
         }
 
     ]
